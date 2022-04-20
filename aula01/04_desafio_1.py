6be2e69ae0a325cb6f22c231655272c14967917d
Beatrizsms <biamagalhaessms@gmail.com>
2022-04-20 19:58:03 -0300

    Primeiros commits - aula 01

--- aula01/04_desafio_1.py
+++ aula01/04_desafio_1.py
@@ -1,3 +1,8 @@
 # 1. Peça ao seu usuário para inserir 3 dados: nome, idade e cidade.
+nome= input("Escreva seu nome: ")
+idade = input("Qual a sua idade?")
+cidade = input("Qual é a sua cidade?")
 
 # 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
+
+print(" Olá " + nome + ", aqui registramos que sua idade é " + idade + " e você é de " + cidade + " . Seja bem-vindo(a)." )
--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,7 +1,12 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
 
+cidade = input("Qual a sua cidade?")
+estado = input("Qual o seu estado?")
+
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
 #    O nome da cidade deve estar todo em letras maiúsculas.
+
+print("Sua cidade é: " + cidade.upper())
 
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
 #    O nome do estado deve estar todo em letras maiúsculas.
